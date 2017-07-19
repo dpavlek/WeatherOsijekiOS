3aed39b0cfa2e5489ea88ae7dea4d2bbb7774420
COBE Osijek <daniel.pavlekovic@icloud.com>
2017-07-19 14:45:10 +0200

    App working.

---
+++ WeatherOsijek/JsonGetter.swift
@@ -0,0 +1,93 @@
+//
+//  JsonGetter.swift
+//  WeatherOsijek
+//
+//  Created by COBE Osijek on 19/07/2017.
+//  Copyright © 2017 COBE Osijek. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct Weather {
+    
+    var weather: (main: String, description: String, icon: String)
+    var main: (temp: Double, pressure: Double, humidity: Double, temp_min: Double, temp_max: Double)
+    var visibility: Int
+    var wind: (speed: Double, deg: Double)
+    
+    init(){
+        weather = ("Sunny", "Sun is shining", "")
+        main = (0,0,0,0,0)
+        visibility = 0
+        wind = (0,0)
+    }
+    
+    mutating func parse(url: String){
+        if let url = URL(string: url) {
+            if let data = try? Data(contentsOf: url) {
+                let json = JSON(data: data)
+                if json["cod"].intValue == 200 {
+                    parser(json: json)
+                }
+            }
+        }
+    }
+    
+    private mutating func parser(json: JSON) {
+        for result in json["weather"].arrayValue {
+            let weather_main = result["main"].stringValue
+            let weather_desc = result["description"].stringValue
+            let weather_icon = result["icon"].stringValue
+            self.weather = (weather_main, weather_desc,weather_icon)
+        }
+        
+        let main_temp = json["main"]["temp"].doubleValue
+        let main_pressure = json["main"]["pressure"].doubleValue
+        let main_humidity = json["main"]["humidity"].doubleValue
+        let main_temp_min = json["main"]["temp_min"].doubleValue
+        let main_temp_max = json["main"]["temp_max"].doubleValue
+        self.main = (main_temp, main_pressure, main_humidity, main_temp_min, main_temp_max)
+        
+        self.visibility = json["visibility"].intValue
+        self.wind = (json["wind"]["speed"].doubleValue, json["wind"]["deg"].doubleValue)
+    }
+}
+
+    /*init?(json: [String: Any]) {
+        guard let main = json["main"] as? [String: Double],
+            let temp = main["temp"],
+            let pressure = main["pressure"],
+            let humidity = main["humidity"],
+            let temp_min = main["temp_min"],
+            let temp_max = main["temp_max"],
+            let visibility = json["visibility"] as? Int,
+            let wind = json["wind"] as? [String: Double],
+            let speed = wind["speed"],
+            let deg = wind["deg"]
+        else {
+            return nil
+        }
+        self.main = (temp, pressure, humidity, temp_min, temp_max)
+        self.visibility = visibility
+        self.wind = (speed, deg)
+    }
+}
+ 
+extension Weather {
+ 
+    static func weathers(query: String, url: String, completion: ([Weather]) -> Void) {
+        var searchURLComponents = URLComponents(string: url)
+        var session = URLSession()
+ 
+        searchURLComponents?.queryItems = [URLQueryItem(name: "q", value: query)]
+        session.dataTask(with: (searchURLComponents?.url)!){(_,_,data,_)
+            var weathers: [Weather] = []
+ 
+            if let data = data,
+                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:Any]{
+ 
+            }
+        }
+    }
+}*/
--- WeatherOsijek/ViewController.swift
+++ WeatherOsijek/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherDesc: UILabel!
+    @IBOutlet weak var weatherTemp: UILabel!
+    @IBOutlet weak var weatherWind: UILabel!
+    @IBOutlet weak var forecastTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let parsingURL = "http://api.openweathermap.org/data/2.5/weather?q=Osijek,hr&units=metric&appid=913cd011c39c592225373dd9d19f62b3"
+        var weatherObject = Weather()
+        weatherObject.parse(url: parsingURL)
+        
+        weatherDesc.text = weatherObject.weather.description.capitalized
+        weatherTemp.text = "Temperature: \(weatherObject.main.temp) °C"
+        weatherWind.text = "Wind speed: \(weatherObject.wind.speed) km/h"
+        
+        let imageURLString = "http://openweathermap.org/img/w/" + weatherObject.weather.icon + ".png"
+        if let imageURL = URL(string: imageURLString){
+            if let imageData = try? Data(contentsOf: imageURL){
+                weatherImage.image = UIImage(data: imageData)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
